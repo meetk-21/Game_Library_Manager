e6278eb64e1113e1c50b65e2777cc43ba099cbf9
meet <meetm9087@gmail.com>
2025-04-24 19:03:40 +0100

    Created SinglePlayerGame

---
+++ SinglePlayerGame.java
@@ -0,0 +1,18 @@
+public class SinglePlayerGame extends AbstractGame implements Playable {
+    private int levelsCompleted;
+
+    public SinglePlayerGame(String title, String genre, String platform, int releaseYear, String developer) {
+        super(title, genre, platform, releaseYear, developer);
+        this.levelsCompleted = 0;
+    }
+
+    @Override
+    public void updateProgress() {
+        levelsCompleted++;
+    }
+
+    @Override
+    public void trackProgress() {
+        System.out.println("Levels completed: " + levelsCompleted);
+    }
+}
